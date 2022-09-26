39fd812d3be5c74f06ddf3ea5247ff4f441b033e
Your Name <you@example.com>
2022-09-26 10:34:02 -0500

    updated index.js file inside of server folder

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,20 @@
-const express = require("express")
-const app = express()
-app.use(express.json())
-const PORT = 3001
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/routes");
+const cors = require("cors");
+const app = express();
 
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/cardinals", router);
 
+mongoose.connect(
+    "mongodb+srv://toinsola:showtime123@cluster0.f8mfvtu.mongodb.net/STLCardinals?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(3001);
+  })
+  .catch((err) => console.log(err));
 
-app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`)
-})
