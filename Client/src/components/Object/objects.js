628ec86d6725e22317d69d00136734b80a015a24
Your Name <you@example.com>
2022-09-26 11:02:36 -0500

    Created objects.js file inside of Object folder.

---
+++ Client/src/components/Object/objects.js
@@ -0,0 +1,31 @@
+
+
+import React, { useEffect, useState } from "react";
+import "./object.css";
+import axios from "axios";
+import Object_function from "./object";
+const URL = "http://localhost:3001/cardinals/view";
+const fetchHandler = async () => {
+  return await axios.get(URL).then((res) => res.data);
+};
+const Objects = () => {
+  const [objects, setObjects] = useState();
+  useEffect(() => {
+    fetchHandler().then((data) => setObjects(data.objects));
+  }, []);
+  console.log(objects);
+  return (
+    <div>
+      <ul>
+        {objects &&
+          objects.map((object, i) => (
+            <li key={i}>
+              <Object object={object} />
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Objects;
